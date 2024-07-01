f9ccb7af2ab80a9099df6c7e3e5961def939eb0e
Caroline Vieira <carolinesanvieira@gmail.com>
2024-07-01 22:32:46 +0200

    feat: config onesignal - android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+import { OneSignal } from 'react-native-onesignal';
+
+
+OneSignal.initialize("4abbfec4-2a63-4790-8b38-9ca86b9a404d")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
