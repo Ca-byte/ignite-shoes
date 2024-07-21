36cb5320d3e760ca8176c8cf7a62123aa07f585f
Caroline Vieira <carolinesanvieira@gmail.com>
2024-07-21 22:53:17 +0200

    receive foreground messge

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
-import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from 'native-base';
 
+import { useEffect } from 'react';
+import { NotificationWillDisplayEvent, OneSignal } from 'react-native-onesignal';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
@@ -9,6 +11,23 @@ export function Routes() {
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const handleNotification = (e: NotificationWillDisplayEvent): void => {
+      e.preventDefault()
+        const response = e.getNotification()
+        console.log(response)
+      
+    }
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay", 
+      handleNotification
+    )
+
+    return () => OneSignal.Notifications.removeEventListener(
+      "foregroundWillDisplay", 
+      handleNotification
+    )
+  }, [])
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
